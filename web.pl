11fa5159eeb390284d8be85f1a8661c0901b82eb
John Turkson <github@johnturkson.com>
2019-12-01 23:09:39 -0800

    Added course data

--- web.pl
+++ web.pl
@@ -3,8 +3,9 @@
 :- use_module(library(persistency)).
 :- use_module(library(http/http_client)).
 
-get(Data) :- http_get("https://m454hj0nc0.execute-api.ca-central-1.amazonaws.com/default/helloworldtest", Data, []).
+get_sections(SectionData) :- http_get("https://ngo8sjao28.execute-api.ca-central-1.amazonaws.com/default/get_section_data_rdf", SectionData, []).
+get_courses(CourseData) :- http_get("https://ngo8sjao28.execute-api.ca-central-1.amazonaws.com/default/get_course_data_rdf", CourseData, []).
 
 save(Data, File) :- open(File, write, Stream), write(Stream, Data), close(Stream).
 
-start :- get(Data), save(Data, "courses.pl"), consult("courses.pl").
+start :- get_sections(SectionData), get_courses(CourseData), save(SectionData, "sections.pl"), save(CourseData, "courses.pl"), consult("sections.pl"), consult("courses.pl").
